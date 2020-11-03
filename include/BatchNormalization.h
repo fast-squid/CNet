b99f9a2e0baa5d2655e5ab6504bb1e6260cfacd1
fast-squid <dlwjdaud@gmail.com>
2020-11-04 00:05:29 +0900

    BN implementation

--- include/BatchNormalization.h
+++ include/BatchNormalization.h
@@ -3,12 +3,44 @@
 #include "DataStruct.h"
 
 // E -> moving-Mean
-// Var -> mobing-Variance
-
+// Var -> moving-Variance
+// 
 // y = (gamma/( root( Var[x] + eps) ))*x + beta - gamma*E[x]/ ( root( Var[x] + eps ) ) 
 // channel wise -> mean and var of batch*height*width
 
-void BatchNorm(ds* X, D_type* gamma, D_type* beta, D_type eps, D_type* moving_mean, D_type* moving_var)
+void BatchNorm(ds* X, D_type* gamma, D_type* beta, D_type eps, D_type* moving_mean, D_type* moving_var, ds* Y)
 {
+	Y->out_channel = X->out_channel;
+	Y->in_channel = X->in_channel;
+	Y->height = X->height;
+	Y->width = X->width;
+	Y->data = (D_type*)malloc(sizeof(X->data));
 
+	int size = X->out_channel*X->in_channel;
+	D_type* factorA = (D_type*)malloc(sizeof(D_type)*size);
+	
+	for(int i = 0; i < size; i++)
+	{
+		factorA[i] = gamma[i]/sqrt(moving_var[i] + eps);
+	}
+
+	for(int oc = 0; oc<X->out_channel; oc++)
+    {
+        for(int ic=0; ic<X->in_channel; ic++)
+        {
+            for(int h=0; h<X->height; h++)
+            {
+                for(int w=0; w<X->width; w++)
+                {
+                    int data_index= oc*X->in_channel*X->height*X->width 
+						+ ic*X->height*X->width 
+						+ h*X->width 
+						+ w;
+					int c_index = oc*X->in_channel
+						+ic;
+                    Y->data[index] = factorA[c_index]*(X->data[index] - moving_mean[c_index]) + beta[c_index];
+                }
+            }
+        }
+    }
 }
